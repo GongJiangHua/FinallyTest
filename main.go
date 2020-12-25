5f9b409c0b1c754dc191bdc3023d2b499a0f31c4
GongJiangHua <1914805653@qq.com>
2020-12-25 10:41:28 +0800

    1222222

--- main.go
+++ main.go
@@ -4,31 +4,16 @@ import "fmt"
 
 func main() {
 	fmt.Println("龚江华")
-<<<<<<< HEAD
 	fmt.Println("欧阳")
-=======
 	fmt.Println("谁再乱提交就完了")
-<<<<<<< HEAD
 	fmt.Println("龚江华sweetbaby")
 	fmt.Println("彭强sweetbaby")
 	fmt.Println("蔡京")
 	fmt.Println("龚江华second")
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> master
 	fmt.Println("啊这")
-=======
-=======
 	fmt.Println("谁如果乱提交就完了")
->>>>>>> gjh
->>>>>>> remotes/origin/master
-=======
-=======
 	fmt.Println("谁如果乱提交就完了")
->>>>>>> gjh
-=======
->>>>>>> master
 	fmt.Println("啊这")
->>>>>>> remotes/origin/ouyang
->>>>>>> master
+	fmt.Println("1225龚江华")
+	fmt.Println("1225龚江华")
 }
